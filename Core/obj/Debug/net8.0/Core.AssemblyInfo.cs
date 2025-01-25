ff485f82a5b512e9692004d036c32e13090afdc6
Daniel Salazar <dsnile.salazr@gmail.com>
2025-01-25 14:59:20 -0600

    Agregar archivos de configuración y ajustes para la aplicación

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b921e4b77db76d153392f5708172e64ebb9c76eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9db9b4b305f9319c453fddf5e89a3797963f7a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
