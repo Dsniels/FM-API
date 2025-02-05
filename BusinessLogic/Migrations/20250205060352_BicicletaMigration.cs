d6105aa39d951bd7385d93e1acba35aa2cc925ef
Daniel Salazar <dsnile.salazr@gmail.com>
2025-02-05 00:32:31 -0600

    Agregar migración para las tablas Bicicleta, Usuario y Renta, eliminando la tabla Carrera

---
+++ BusinessLogic/Migrations/20250205060352_BicicletaMigration.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessLogic.Migrations
+{
+    /// <inheritdoc />
+    public partial class BicicletaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Carrera");
+
+            migrationBuilder.CreateTable(
+                name: "Bicicleta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Disponible = table.Column<bool>(type: "bit", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bicicleta", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrimerApellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SegundoApellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Matricula = table.Column<int>(type: "int", nullable: false),
+                    HorarioID = table.Column<int>(type: "int", nullable: false),
+                    Alumno = table.Column<bool>(type: "bit", nullable: false),
+                    Admin = table.Column<bool>(type: "bit", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Renta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Entregado = table.Column<bool>(type: "bit", nullable: false),
+                    FechaRenta = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    FechaEntrega = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UsuarioID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    BicicletaID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Renta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Renta_Usuario_UsuarioID",
+                        column: x => x.UsuarioID,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Renta_UsuarioID",
+                table: "Renta",
+                column: "UsuarioID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bicicleta");
+
+            migrationBuilder.DropTable(
+                name: "Renta");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.CreateTable(
+                name: "Carrera",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Abreviatura = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrera", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carrera_Nombre_Abreviatura",
+                table: "Carrera",
+                columns: new[] { "Nombre", "Abreviatura" },
+                unique: true);
+        }
+    }
+}
